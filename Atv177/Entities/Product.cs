f7b23d1010d4a004896c9e565539a285a7f82d1e
MEGA <MEGA@BRENDA-PC>
2026-05-26 16:45:47 -0300

    Adicionar arquivos de projeto.

---
+++ Atv177/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atv177.Entities
+{
+     class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Product (string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
